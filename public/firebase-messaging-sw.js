0d531c3b6a108ec4861fefb1e6be58a8b427e04c
Asteroid-PX <merch.abdalrhman@gmail.com>
2025-08-24 09:18:59 +0300

    lsajflsajdfl

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -5,25 +5,14 @@ importScripts(
   "https://www.gstatic.com/firebasejs/10.13.2/firebase-messaging-compat.js"
 );
 
-const firebaseConfig = {
-  apiKey: "AIzaSyBdpPLvwyyaXMunlTCR5bm9AHJEaTdbq4Y",
-  authDomain: "gala-app-70ccd.firebaseapp.com",
-  projectId: "gala-app-70ccd",
-  storageBucket: "gala-app-70ccd.firebasestorage.app",
+firebase.initializeApp({
   messagingSenderId: "451026366516",
-  appId: "1:451026366516:web:f81879acddc36fc7f65a61",
-};
-console.log("Service worker Firebase config loaded");
+});
 
-const firebaseApp = firebase.initializeApp(firebaseConfig);
-
-// Retrieve an instance of Firebase Messaging so that it can handle background
-// messages.
 const messaging = firebase.messaging();
 
 messaging.onBackgroundMessage((payload) => {
   try {
-    // Customize notification here
     const notificationTitle = payload.notification?.title || "New Message";
     const notificationOptions = {
       body: payload.notification?.body || "You have a new message",
